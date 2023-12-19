c9256b03d2a8851a438ac3092d02e3520e428e6a
Aarni Koskela <akx@iki.fi>
2023-12-19 08:56:40 +0200

    Support pytest.ini too

--- pyproject_migrator/result.py
+++ pyproject_migrator/result.py
@@ -15,19 +15,40 @@ class Result:
         log.warning(msg)
 
     def assign(self, key: str, value: object) -> None:
-        dest = self.dest
-        bits = key.split(".")
-        final_key = bits.pop()
-        for bit in bits:
-            dest = dest.setdefault(bit, {})
+        dest, final_key = self._find_dest(key)
         if final_key in dest:
             raise ValueError(f"Key {key} already exists")
         dest[final_key] = value
 
     def append(self, key: str, value: object) -> None:
+        dest, final_key = self._find_dest(key)
+        dest.setdefault(final_key, []).append(value)
+
+    def merge(self, t_key: str, vals: dict, *, path: Path) -> None:
+        dest, final_key = self._find_dest(t_key)
+        dest = dest.setdefault(final_key, {})
+        for key, value in vals.items():
+            if isinstance(value, list):
+                if key in dest:
+                    self.warn(
+                        f"Appending values in {t_key}->{key}; make sure the order is correct",
+                        path,
+                    )
+                dest.setdefault(key, []).extend(value)
+            elif isinstance(value, dict):
+                dest.setdefault(key, {}).update(value)
+            else:
+                if key in dest:
+                    self.warn(
+                        f"Overwriting {t_key}->{key} (previously {dest[key]!r}) with {value!r}",
+                        path,
+                    )
+                dest[key] = value
+
+    def _find_dest(self, key: str) -> tuple[dict, str]:
         dest = self.dest
         bits = key.split(".")
         final_key = bits.pop()
         for bit in bits:
             dest = dest.setdefault(bit, {})
-        dest.setdefault(final_key, []).append(value)
+        return dest, final_key
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,7 +11,10 @@ def test_cli(monkeypatch, capsys, caplog, snapshot):
     caplog.set_level(logging.INFO)
     monkeypatch.setattr("sys.argv", ["tool", str(victim_path)])
     cli()
+    # Snip out pathnames for snapshot comparison...
+    messages = sorted(m.partition(": ")[-1] for m in caplog.messages)
+    assert messages == snapshot(name="log")
     assert "setuptools sections ['metadata', 'options']" in caplog.text
     assert "flake8-related sections ['flake8']" in caplog.text
     parsed = tomlkit.loads(capsys.readouterr()[0])
-    assert parsed == snapshot
+    assert parsed == snapshot(name="result")
